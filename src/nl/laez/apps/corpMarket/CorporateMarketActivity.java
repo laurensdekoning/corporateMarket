a4b19acc1a0474027156897bd96f0f3a3005e60e
Laurens de Koning <laurens@laurensdekoning.nl>
2012-01-11 11:08:05 +0100

    Initial commit

---
+++ src/nl/laez/apps/corpMarket/CorporateMarketActivity.java
@@ -0,0 +1,13 @@
+package nl.laez.apps.corpMarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CorporateMarketActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
