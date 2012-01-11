edeb266cba5a669e9a8fdb24f17cb603791c40f9
Laurens de Koning <laurens@laurensdekoning.nl>
2012-01-11 22:25:30 +0100

    Browser toegevoegd (webView) - Just playing around..

--- gen/nl/laez/apps/corpMarket/R.java
+++ gen/nl/laez/apps/corpMarket/R.java
@@ -10,17 +10,21 @@ package nl.laez.apps.corpMarket;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bgColMainForm=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int spinner1=0x7f050000;
+        public static final int webView1=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int handle=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
--- src/nl/laez/apps/corpMarket/CorporateMarketActivity.java
+++ src/nl/laez/apps/corpMarket/CorporateMarketActivity.java
@@ -2,6 +2,8 @@ package nl.laez.apps.corpMarket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class CorporateMarketActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,13 @@ public class CorporateMarketActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        WebView webViewert = (WebView) findViewById(R.id.webView1);
+        WebSettings webViewertSet = webViewert.getSettings();
+        webViewertSet.setJavaScriptEnabled(true);
+        webViewertSet.setSupportZoom(true);
+        
+        webViewert.loadUrl("http://laez.nl");
+        
     }
 }
